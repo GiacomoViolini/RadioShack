92b203a5c3cc2792e56d27d6dd0c5a426c79a517
GiacomoViolini <giacvio@gmail.com>
2024-03-29 19:30:46 +0100

    Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import "../pagestyles/Home.css"
+import "../pagestyles/Home.css";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -11,60 +11,80 @@ export default function Home() {
     "acquisti",
     "vendite",
   ];
+  const current = new Date();
+  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()}`;
   function navigateTo(url: string) {
     navigate(`/${url}`);
   }
 
   return (
-    <div className="flex flex-col justify-center items-center h-full m-10">
-      <div className="grid grid-cols-3 gap-x-40 gap-y-4">
+    <div className="flex flex-col justify-center h-full m-6">
+      <div className="flex items-center justify-between mb-3">
+        <img src="./public/logo.png" className="w-10 h-10"/>
+        <h2 className="text-lg">{date}</h2>
+      </div>
+      <div className="grid grid-cols-3 gap-x-32 gap-y-4">
         <div className="relative flex flex-col items-center">
           <img
             src="./public/Prodotti.svg"
-            className="home-image"
+            className="home-image hover:bg-red-500"
             onClick={() => navigateTo(routes[0])}
           />
-          <h2 className="text-2xl mt-2 font-bold">{routes[0][0].toUpperCase() + routes[0].slice(1)}</h2>
+          <h2 className="home-text">
+            {routes[0][0].toUpperCase() + routes[0].slice(1)}
+          </h2>
         </div>
         <div className="flex flex-col items-center">
           <img
-            src="./public/Prodotti.svg"
-            className="home-image "
+            src="./public/Statistiche.svg"
+            className="home-image hover:bg-blue-500 "
             onClick={() => navigateTo(routes[1])}
           />
-          <h2 className="text-2xl mt-2 font-bold">{routes[1][0].toUpperCase() + routes[1].slice(1)}</h2>
+          <h2 className="home-text">
+            {routes[1][0].toUpperCase() + routes[1].slice(1)}
+          </h2>
         </div>
         <div className="flex flex-col items-center">
           <img
-            src="./public/Prodotti.svg"
-            className="home-image "
+            src="./public/Fornitori.svg"
+            className="home-image hover:bg-emerald-500 "
             onClick={() => navigateTo(routes[2])}
           />
-          <h2 className="text-2xl mt-2 font-bold">{routes[2][0].toUpperCase() + routes[2].slice(1)}</h2>
+          <h2 className="home-text">
+            {routes[2][0].toUpperCase() + routes[2].slice(1)}
+          </h2>
         </div>
         <div className="flex flex-col items-center">
           <img
-            src="./public/Prodotti.svg"
-            className="home-image "
+            src="./public/Clienti.svg"
+            className="home-image hover:bg-amber-500 "
             onClick={() => navigateTo(routes[3])}
           />
-          <h2 className="text-2xl mt-2 font-bold">{routes[3][0].toUpperCase() + routes[3].slice(1)}</h2>
+          <h2 className="home-text">
+            {routes[3][0].toUpperCase() + routes[3].slice(1)}
+          </h2>
         </div>
         <div className="flex flex-col items-center">
           <img
-            src="./public/Prodotti.svg"
-            className="home-image "
+            src="./public/Acquisti.svg"
+            className="home-image hover:bg-pink-500 "
             onClick={() => navigateTo(routes[4])}
           />
-          <h2 className="text-2xl mt-2 font-bold">{routes[4][0].toUpperCase() + routes[4].slice(1)}</h2>
+          <h2 className="home-text">
+            {routes[4][0].toUpperCase() + routes[4].slice(1)}
+          </h2>
         </div>
         <div className="flex flex-col items-center">
           <img
-            src="./public/Prodotti.svg"
-            className="home-image "
+            src="./public/Vendite.svg"
+            className="home-image hover:bg-purple-500"
             onClick={() => navigateTo(routes[5])}
           />
-          <h2 className="text-2xl mt-2 font-bold">{routes[5][0].toUpperCase() + routes[5].slice(1)}</h2>
+          <h2 className="home-text">
+            {routes[5][0].toUpperCase() + routes[5].slice(1)}
+          </h2>
         </div>
       </div>
     </div>
