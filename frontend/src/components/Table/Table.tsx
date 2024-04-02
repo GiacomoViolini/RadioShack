3ab51ae505afcbc173022e4ca3aac55a611494a8
Lorenzo Serloni <S1109161@studenti.univpm.it>
2024-04-02 19:11:50 +0200

    {commit_message}

--- frontend/src/components/Table/Table.tsx
+++ frontend/src/components/Table/Table.tsx
@@ -1,6 +1,7 @@
 import visualizeIcon from "/VisualizeIcon.svg";
 import modifyIcon from "/ModifyIcon.svg";
 import deleteIcon from "/DeleteIcon.svg";
+import axios from "axios";
 
 interface TableProps {
   fields: string[];
@@ -8,6 +9,7 @@ interface TableProps {
 }
 
 interface Fornitori {
+  codice_fornitore: string;
   nome: string;
   email: string;
   telefono: string;
@@ -42,6 +44,16 @@ interface Vendite {
 }
 
 export default function Table({ fields, informations }: TableProps) {
+  const deleteFornitore = async (id) => {
+    try {
+      await axios.delete(`http://localhost:8000/radioapp/deleteFornitore/${id}`);
+      // After deleting, you can update the state to remove the deleted fornitore from the list
+      // Or you can reload the page to get the updated list
+      window.location.reload();
+    } catch (error) {
+      console.error("Failed to delete fornitore:", error);
+    }
+  };
   return (
     <table className="border-spacing-y-1 shadow-sm shadow-slate-300 border text-center border-slate-300 w-full rounded-md overflow-hidden">
       <thead className="bg-slate-100 text-black ">
@@ -59,7 +71,10 @@ export default function Table({ fields, informations }: TableProps) {
       </thead>
       <tbody className="border-spacing-y-1 border xl:text-xs">
         {informations.map((information, rowIndex) => (
-          <tr key={rowIndex} className="border-spacing-y-1 h-12 gap-2 border p-2">
+          <tr
+            key={rowIndex}
+            className="border-spacing-y-1 h-12 gap-2 border p-2"
+          >
             {Object.entries(information).map(([key, info]) => (
               <td key={key} className="mx-2">
                 {info instanceof Date ? info.toLocaleString() : info}
@@ -77,7 +92,7 @@ export default function Table({ fields, informations }: TableProps) {
                 <button>
                   <img src={modifyIcon} alt="modify"></img>
                 </button>
-                <button>
+                <button onClick={() => deleteFornitore(informations.codice_fornitore)}>
                   <img src={deleteIcon} alt="delete" className="ml-1"></img>
                 </button>
               </td>
