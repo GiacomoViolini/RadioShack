0dcea03c3202e371081314b6a708082313186c9c
GiacomoViolini <giacvio@gmail.com>
2024-04-05 22:28:55 +0200

    Merging

---
+++ frontend/src/components/CustomChart/CustomChart.tsx
@@ -0,0 +1,95 @@
+import { Chart, registerables } from "chart.js";
+import { useState, useEffect } from "react";
+import { CustomChartsProps } from "../../interfaceHelper";
+import { AssignColor } from "../../utils";
+
+Chart.register(...registerables); // Register all scale types
+
+export default function CustomChart({
+  XPairs,
+  YScale,
+  Label,
+  chartRef,
+  Category,
+}: CustomChartsProps) {
+  const [BackColor, setBackColor] = useState<string[]>([]);
+  const [BorColor, setBorColor] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (XPairs && Category) {
+      let auxTuple: [string[], string[]] = AssignColor(XPairs, Category);
+      setBackColor(auxTuple[0]);
+      setBorColor(auxTuple[1]);
+    }
+  }, [XPairs, Category]);
+
+  useEffect(() => {
+    const canvas = document.getElementById(Label) as HTMLCanvasElement;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    if (chartRef.current) {
+      chartRef.current.destroy();
+    }
+
+    chartRef.current = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: XPairs.map((XPair) => XPair[0]),
+        datasets: [
+          {
+            label: Label,
+            data: XPairs.map((XPair) => XPair[1]),
+            backgroundColor: BackColor,
+            borderColor: BorColor,
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          x: {
+            ticks: {
+              color: "white",
+            },
+            grid: {
+              color: "white",
+            },
+          },
+          y: {
+            beginAtZero: true,
+            min: YScale[0],
+            max: YScale[1],
+            ticks: {
+              color: "#FFFFFF",
+            },
+            grid: {
+              color: "white",
+            },
+          },
+        },
+        plugins: {
+          legend: {
+            labels: {
+              color: "white",
+            },
+          },
+        },
+        elements: {
+          point: {
+            backgroundColor: "#FFFFFF",
+          },
+        },
+        maintainAspectRatio: false,
+      },
+    });
+  }, [XPairs, Category, BackColor, BorColor]);
+
+  return (
+    <div className="container w-full h-80 2xl:h-5/6 bg-none rounded-md">
+      <canvas id={Label}></canvas>
+    </div>
+  );
+}
---
+++ frontend/src/components/Filter/StatisticheFilter.tsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import "../../pagestyles/Prodotti.css";
+import { FilterPropsStatistiche } from "../../interfaceHelper";
+
+export default function StatisticheFilter({
+  title,
+  options,
+  setProdottiOption,
+  setFornitoriOption,
+  setClientiOption,
+}: FilterPropsStatistiche) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [checkedOption, setCheckedOption] = useState(options[0]);
+
+  useEffect(() => {
+    setCheckedOption(options[0]);
+    handleCheck(options[0]);
+  }, []);
+
+  const handleCheck = (option: string) => {
+    setCheckedOption(option);
+    switch (title) {
+      case "Top 10 dei Prodotti":
+        setProdottiOption(option);
+        break;
+      case "Top 10 dei Fornitori":
+        setFornitoriOption(option);
+        break;
+      case "Top 10 dei Clienti":
+        setClientiOption(option);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-between items-center">
+        <h2 className="text-lg ">{title}</h2>
+        <button className="p-1 rounded-full" onClick={() => setIsOpen(!isOpen)}>
+          <img
+            id="arrow"
+            src={"/Down.svg"}
+            className={`w-4 h-4 ${isOpen ? "rotate-up" : "rotate-down"}`}
+          />
+        </button>
+      </div>
+      <div className={`filter-content ${isOpen ? "open" : "closed"} mb-2`}>
+        {options.map((option, index) => (
+          <div key={option} className="flex items-center gap-2">
+            <input
+              type="checkbox"
+              className="accent-white"
+              onChange={() => handleCheck(option)}
+              checked={option === checkedOption}
+            />
+            <label>{option}</label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
